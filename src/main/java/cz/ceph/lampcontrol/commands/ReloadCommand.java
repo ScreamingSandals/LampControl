9d7e071a0b1758e5a8546076e8daf3c90e901bb0
Ceph <granik24@hotmail.cz>
2017-01-16 00:22:17 +0100

    Added reload command

--- src/main/java/cz/ceph/lampcontrol/commands/ReloadCommand.java
+++ src/main/java/cz/ceph/lampcontrol/commands/ReloadCommand.java
@@ -1,8 +1,47 @@
 package cz.ceph.lampcontrol.commands;
 
+import cz.ceph.lampcontrol.commands.core.IBasicCommand;
+import cz.ceph.lampcontrol.commands.core.RegisterCommand;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import static cz.ceph.lampcontrol.LampControl.getMain;
+
 /**
  * Created by Ceph on 10.01.2017.
  */
 
-public class ReloadCommand {
+@RegisterCommand(value = "lampreload", alias = "reloadlamp")
+public class ReloadCommand implements IBasicCommand {
+
+    @Override
+    public String getPermission() {
+        return "lampcontrol.command.on";
+    }
+
+    @Override
+    public String getDescription() {
+        return getMain().getLocalizations().get(getMain().language, "command.on_lamp_description");
+    }
+
+    @Override
+    public String getUsage() {
+        return "/onlamp or /lampon";
+    }
+
+    @Override
+    public boolean onConsoleCommand(ConsoleCommandSender sender, String[] args) {
+        getMain().getMainConfig().initializeConfig();
+        sender.sendMessage(getMain().getLocalizations().get(getMain().language,"info.reloaded"));
+
+        return true;
+    }
+
+    @Override
+    public boolean onPlayerCommand(Player player, String[] args) {
+        getMain().getMainConfig().initializeConfig();
+        player.sendMessage(getMain().getLocalizations().get(getMain().language,"info.reloaded"));
+
+        return false;
+    }
 }
